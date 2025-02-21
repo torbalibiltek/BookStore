8f7ef4fb7cf197125e5d51a97fad8b39b6b2f952
Mustafa KURAL <mustafakural@outlook.com>
2025-02-21 11:27:57 +0300

    dbcontext oluşturuldu, migration eklendi

---
+++ Db/KitapContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Db
+{
+    public class KitapContext : DbContext
+    {
+        public DbSet<Kitap> Kitaplar { get; set; }
+        public DbSet<Kategori> Kategoriler { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = bookstore.db;");
+        }
+    }
+}
